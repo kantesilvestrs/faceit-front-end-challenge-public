cfea5deb356059d51fae293095efe3108c618246
Silvestrs Kante <silvestrs@wizardsdc.com>
2020-06-27 19:04:47 +0100

    Moved App into core module

---
+++ src/core/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container, H4 } from '../ui/components';
+
+export const App = () => (
+  <Container>
+    <H4>FACEIT Tournaments</H4>
+  </Container>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { GlobalStyle } from './ui/GlobalStyle';
 import { store } from './core/store';
-import { Container } from './ui/components';
-import { H4 } from './ui/components';
 import { ThemeProvider } from 'styled-components';
 import theme from './ui/theme/theme';
-
-const App: React.FC = () => {
-  return (
-    <Container>
-      <H4>FACEIT Tournaments</H4>
-    </Container>
-  );
-};
+import { App } from './core/App';
 
 ReactDOM.render(
   <Provider store={store}>

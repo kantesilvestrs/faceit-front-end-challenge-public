7bd0a228b95103385f11cc74877bb78f07c2d072
Silvestrs Kante <silvestrs@wizardsdc.com>
2020-06-28 14:00:48 +0100

    Wrapped main App with core application dynamic store module

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -5,11 +5,17 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import { store } from './store';
 import { theme } from '../ui/theme/theme';
+import { DynamicModuleLoader } from 'redux-dynamic-modules';
+import { getCoreApplicationStoreModule } from './store/storeModule';
+
+const coreApplicationModule = getCoreApplicationStoreModule();
 
 const App = () => (
-  <Container>
-    <H4>FACEIT Tournaments</H4>
-  </Container>
+  <DynamicModuleLoader modules={[coreApplicationModule]}>
+    <Container>
+      <H4>FACEIT Tournaments</H4>
+    </Container>
+  </DynamicModuleLoader>
 );
 
 const AppWrapper = () => (
